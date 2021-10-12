89affc4244eb83ade7591640bbe426249d225ad4
EliasBoaroli <72479032+EliasBoaroli@users.noreply.github.com>
2021-10-12 16:28:19 -0300

    commit config inicial

---
+++ src/main/java/br/senac/ecommerceApiComercial/EcommerceApiComercialApplication.java
@@ -0,0 +1,13 @@
+package br.senac.ecommerceApiComercial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceApiComercialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceApiComercialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/senac/ecommerceApiComercial/EcommerceApiComercialApplicationTests.java
@@ -0,0 +1,13 @@
+package br.senac.ecommerceApiComercial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceApiComercialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
